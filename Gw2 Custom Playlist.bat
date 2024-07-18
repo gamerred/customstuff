ab7ccd5f1354130db2aee3ab1e48e290a27465aa
gamerred <gamerredx@gmail.com>
2024-07-18 12:49:27 -0400

    Add files via upload
    must right click and edit with text editor and replace "myfolder" with your games folder a example is C:\Users\name\OneDrive\Documents\Guild Wars 2\Documents\Guild Wars 2 its diff per user

---
+++ Gw2 Custom Playlist.bat
@@ -0,0 +1,84 @@
+@echo off
+color 02
+set _MAINPATH_=%~dp0
+set _MPATH_=%_MAINPATH_%CustomPlaylist\
+set CUSTOM=myfolder
+
+echo #########################################
+echo ###### GW2 Custom Playlist Rev 2.0 ######
+echo ######     by TheCheatsrichter     ######
+echo #########################################
+
+echo.
+echo Make sure the path is right
+echo Trying path %CUSTOM%\
+echo This batch will automatically create folders
+echo in which you can place your custom audio files
+echo for Guild Wars 2.
+echo.
+echo Supported formats are:
+echo 	*.mp3 *.aiff *.flac *.ogg *.wav
+echo.
+echo.
+echo This batch will:
+echo 	*)create subfolders for each playlist
+echo 	*)generate playlists in .m3u formats
+echo 	*)setup the playlists to work with GuildWars2
+echo.
+echo 	Press any key to start by creating the Playlist folders
+pause
+
+
+:makedir
+echo [+] Building DIR structure ...
+If NOT EXIST "%_MPATH_%" mkdir "%_MPATH_%"
+If NOT EXIST "%_MPATH_%#Playlists" mkdir "%_MPATH_%#Playlists"
+
+FOR %%G IN (Ambient Battle BossBattle City Defeated MainMenu NightTime Underwater Victory) DO (IF NOT EXIST "%_MPATH_%%%G" (mkdir "%_MPATH_%%%G" & echo [+] %%G folder created! ) else ( echo [*] Folder %%G allready created))
+echo.
+echo.
+echo [!] Please insert your Audiofiles!
+pause
+
+:makeplaylists
+FOR %%G IN (Ambient Battle BossBattle City Defeated MainMenu NightTime Underwater Victory) DO (
+	chdir "%_MPATH_%%%G"
+	echo [+] Building %%G.m3u ...
+	dir /o:n/a/b/s *.mp3 *.aiff *.flac *.ogg *.wav >"%_MPATH_%#Playlists\%%G.m3u" || (del "%_MPATH_%#Playlists\%%G.m3u" && echo [*] %%G folder is empty...)
+	)
+	
+echo.
+echo.
+echo [+] Playlists Successfully created!
+echo.
+:readapply
+set /p w=[*]Do you want to apply them now [y/n]?:
+
+:copyplaylists
+If NOT EXIST "%CUSTOM%\" (
+	color 0C
+	echo [-] %CUSTOM%\ not found!
+	echo [-] Please manually copy the playlist files or force copy process 
+	set /p w=[*]Force copy process [y/n]?:
+	if not "%w%" == "y" (
+		echo [-] Closing batch...
+		goto eof
+	)
+	
+)
+If NOT EXIST "%CUSTOM%\Music" mkdir "%CUSTOM%\Music"
+
+
+
+if "%w%" == "y" (
+	xcopy /y /s "%_MPATH_%#Playlists" "%CUSTOM%\Music"  > nul
+	echo [+] Playlists successfully applied!
+	echo [+] Please restart GuildWars2 if its currently running!
+	echo [+] Have fun! Greetings GamerRed25!
+	color 0A
+	) else (
+	echo.
+	echo [-] Playlists not applied!
+	color 0C
+	)
+pause
